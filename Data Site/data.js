a24380b607e38b9de86d47f8951579a682826cd5
rachellemenn <32510583+rachellemenn@users.noreply.github.com>
2018-02-16 18:57:06 +0000

    pictures won't load
    pls load

--- Data Site/data.js
+++ Data Site/data.js
@@ -12,14 +12,18 @@ var button = document.getElementById("button3");
 // vizDiv.style.visibility = "hidden";
 // vizDiv.innerHTML = "";
 
-// button.addEventListener("click", checkResult);
 
 //initalise home screen
 contentDiv.innerHTML = "The content of this page will be regularly updated with more <br><br> complex versions of existing visualisations and new projects. <br><br><br> The buttons below can be used to view visualisations independently. <br><br> Alternatively, complete data stories can be viewed using the nav bar.";
 // button.value = "FIRST STORY";
 
 function show() {
+  var button = document.getElementById("button1").innerHTML = '<img src="viz/Story1Data1.png" alt="">';
   var button = document.getElementById("button1").style.maxHeight = "200px";
+  var button = document.getElementById("button2").innerHTML= '<img src="viz/Story2Data1.jpg" alt="">';
   var button = document.getElementById("button2").style.maxHeight = "200px";
+  var button = document.getElementById("button3").innerHTML= '<img src="viz/Story2Data2.png" alt="">';
   var button = document.getElementById("button3").style.maxHeight = "200px";
 }
+
+button.addEventListener("click", show);
