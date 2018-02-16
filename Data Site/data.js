fd755da542daf5902ef736408c63b90e949d1304
rachellemenn <32510583+rachellemenn@users.noreply.github.com>
2018-02-16 19:13:58 +0000

    calls the function
    still doesn't work

--- Data Site/data.js
+++ Data Site/data.js
@@ -19,11 +19,12 @@ contentDiv.innerHTML = "The content of this page will be regularly updated with 
 
 function show() {
   var button = document.getElementById("button1").innerHTML = '<img src="viz/Story1Data1.png" alt="">';
-  var button = document.getElementById("button1").style.maxHeight = "200px";
-  var button = document.getElementById("button2").innerHTML= '<img src="viz/Story2Data1.jpg" alt="">';
-  var button = document.getElementById("button2").style.maxHeight = "200px";
-  var button = document.getElementById("button3").innerHTML= '<img src="viz/Story2Data2.png" alt="">';
-  var button = document.getElementById("button3").style.maxHeight = "200px";
+  var content = document.getElementById("button1").style.maxHeight = "200px";
+  // var button = document.getElementById("button2").innerHTML= '<img src="viz/Story2Data1.jpg" alt="">';
+  // var button = document.getElementById("button2").style.maxHeight = "200px";
+  // var button = document.getElementById("button3").innerHTML= '<img src="viz/Story2Data2.png" alt="">';
+  // var button = document.getElementById("button3").style.maxHeight = "200px";
 }
 
-button.addEventListener("click", show);
+
+show();
