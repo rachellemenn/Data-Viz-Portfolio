9e471130af140b08efb9b67deb6f554c01817f0a
rachellemenn <32510583+rachellemenn@users.noreply.github.com>
2018-02-16 20:20:00 +0000

    buttons work
    images too big

--- Data Site/data.js
+++ Data Site/data.js
@@ -18,20 +18,30 @@ var button3Div= document.getElementById("button3");
 // vizDiv.innerHTML = "";
 
 
+
 //initalise home screen
 contentDiv.innerHTML = "The content of this page will be regularly updated with more <br><br> complex versions of existing visualisations and new projects. <br><br><br> The buttons below can be used to view visualisations independently. <br><br> Alternatively, complete data stories can be viewed using the nav bar.";
 // button.value = "FIRST STORY";
 
-function show() {
-  var newImage = document.getElementById("content").innerHTML = '<img src="viz/Story1Data1.png" alt="">';
-  // var newImage = document.getElementById("content").style.height = "200px";
-  var newImage1 = document.getElementById("content").innerHTML= '<img src="viz/Story2Data1.jpg" alt="">';
+function show1() {
+  var image1 = document.getElementById("content").innerHTML = '<img src="viz/Story1Data1.png" alt="">';
+  //var newImage = document.getElementById("content").style.height = "200px";
+}
+function show2(){
+  var image2 = document.getElementById("content").innerHTML= '<img src="viz/Story2Data1.jpg" alt="">';
   // var newImage1 = document.getElementById("button2").style.maxHeight = "200px";
-  var newImage2 = document.getElementById("content").innerHTML= '<img src="viz/Story2Data2.png" alt="">';
+}
+function show3(){
+  var image3 = document.getElementById("content").innerHTML= '<img src="viz/Story2Data2.png" alt="">';
   // var newImage2 = document.getElementById("button3").style.maxHeight = "200px";
 }
 
-button1.addEventListener("click", show);
-button2.addEventListener("click", show);
-button3.addEventListener("click", show);
-show();
+// image3.style.visibility = "hidden";
+// image3.innerHTML = "";
+
+button1Div.addEventListener("click", show1);
+// show1();
+button2Div.addEventListener("click", show2);
+// show2();
+button3Div.addEventListener("click", show3);
+// show3();
